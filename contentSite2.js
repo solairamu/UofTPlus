60058edc017d2a3c7a27ea8b4da39660e46e7620
solairamu <solairamu18@gmail.com>
2024-01-16 20:16:58 -0500

    minor change

--- contentSite2.js
+++ contentSite2.js
@@ -20,7 +20,6 @@
         var tableBody = document.querySelector('#table > table > tbody');
         const courseData = [];
         for (const row of tableBody.rows) {
-            // Adjust the indices according to your table's structure
             const semester = row.cells[2].innerText.trim();
             const average = row.cells[4].innerText.trim();
             courseData.push({ semester, average });
@@ -31,7 +30,5 @@
         console.log(courseData);
         }
     }
-    
-    // Call this function after the search results are loaded, you might need to set a timeout or use a mutation observer to wait for the data to load.
-    
+        
 })();
