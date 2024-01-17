2348455a71edba0cd4ee3356e178df8030f47973
solairamu <solairamu18@gmail.com>
2024-01-17 01:56:33 -0500

    bug fixes

--- popup.js
+++ popup.js
@@ -1,17 +1,20 @@
 function updatePopupContent() {
     chrome.runtime.sendMessage({ action: "fetchCurrentCourseInfo" }, function(response) {
         const courseInfoDiv = document.getElementById('courseInfo');
-        if (response && response.data && response.data.length > 0) {
-            courseInfoDiv.innerHTML = ''; // Clear existing content
+        const contentDiv = document.createElement('div'); // Create a new div for the content
 
+        if (response && response.data && response.data.length > 0) {
             response.data.forEach(info => {
-                courseInfoDiv.innerHTML += `Code: ${info.Code}, Semester: ${info.Semester}, Average: ${info.Average}<br>`;
+                contentDiv.innerHTML += `Code: ${info.Code}, Semester: ${info.Semester}, Average: ${info.Average}<br>`;
             });
         } else {
-            courseInfoDiv.innerHTML = 'No data available';
+            contentDiv.innerHTML = 'No data available';
         }
+
+        courseInfoDiv.appendChild(contentDiv); // Append the new content div to courseInfo
     });
 }
+
 
 // Fetch and update data when the popup is opened
 updatePopupContent();
